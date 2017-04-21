b73245bd43212b073ac8f96229d51f5c74749f9e
toowan <mail@toowan.me>
2017-04-20 17:17:19 -0700

    Added flash notice for selecting membership plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  
+  # Run private select_plan method below when user signs up
+  before_action :select_plan, only: :new
+
   # Extend default Devise gem behavior so that
   # users signing up with the Pro account (plan ID 2)
   # save with a special Stripe subscription function.
@@ -16,5 +18,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
       end
     end
   end
+
+  # User must pick a membership plan when visiting the registration form
+  private 
+    def select_plan 
+      unless (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign-up."
+        redirect_to root_url
+      end
+    end
 
 end
